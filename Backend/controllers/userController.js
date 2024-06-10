a88dcf67dff4810b1898b87608b0d87e5b8f1fb9
osamaSale <osama.mohamad.salem@gmail.com>
2024-06-10 12:04:58 +0300

    commit1

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,43 +1,47 @@
 
 const jwt = require('jsonwebtoken');
-const { getUserByEmail } = require('../models/userModel');
 const userModel = require('../models/userModel');
 
 module.exports = {
     register: async (req, res) => {
         const { name, email, password, role } = req.body;
         if (!name || !email || !password || !role) {
-            return res.status(400).json({ message: 'All fields are required' });
+            return res.json({ message: 'All fields are required' , status: 400 });
         }
         try {
             const user = { name, email, password, role };
             if (user[0]) {
-                return res.status(400).json({ message: "Email already exist" });
+                return res.json({ message: "Email already exist", status: 400 });
             }
             const userId = await userModel.createUser(user);
-            res.status(201).json({ message: 'User created successfully', userId });
+            res.json({ message: 'User created successfully', userId, status: 201 });
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Server Error' });
+            if (error) {
+                if (error.code === 'ER_DUP_ENTRY') {
+                    res.json({ message: 'Email already exists' });
+                }
+                console.error(error);
+            res.status(500).json({ message: 'Server Error'  , status: 500 });
+            }
         }
     },
 
     login: async (req, res) => {
         const { email, password } = req.body;
         if (!email || !password) {
-            return res.status(400).json({ message: 'Email and password are required' });
+            return res.json({ message: 'Email and password are required', status: 400 });
         }
         try {
             const user = await userModel.getUserByEmail(email);
             if (!user) {
-                return res.status(400).json({ message: 'Invalid email or password' });
+                return res.json({ message: 'Invalid email or password', status: 400 });
             }
             const isPasswordValid = await userModel.validatePassword(password, user.password);
             if (!isPasswordValid) {
-                return res.status(400).json({ message: 'Invalid email or password' });
+                return res.json({ message: 'Invalid email or password' , status: 400});
             }
             const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            res.json({ token, user, message: "Login completed successfully" });
+            res.json({ token, user, message: "Login completed successfully" ,status: 201 });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Server Error' });
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# KASSEL
+# KASSELSoft
+
+<!-- Clone this repository using bash terminal: -->
+
+    git clone https://github.com/osamaSale/KASSEL
+
+<!--End clone area -->
